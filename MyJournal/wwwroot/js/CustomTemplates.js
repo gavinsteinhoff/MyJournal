442076e19947de4631619bdcc35fb201f295ad89
Gavin Steinhoff <gavinsteinhoff@gmail.com>
2019-02-18 14:32:49 -0600

    refactored template engine

---
+++ MyJournal/wwwroot/js/CustomTemplates.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    let select = $("#template");
+
+    $.getScript("../js/templates/templateA.js", function () { });
+    select.append('<option value="A">Template A</option>');
+
+    $.getScript("../js/templates/templateB.js", function () { });
+    select.append('<option value="B">Template B</option>');
+    
+    select.append('<option value="C">Template C</option>');
+    $.getScript("../js/templates/templateC.js", function () { });
+
+    $("select#template").change(function () {
+        var template = $(this).children("option:selected").val();
+        switch (template) {
+            case "A":
+                $('#JournalText').val(templateA);
+                break;
+            case "B":
+                $('#JournalText').val(templateB);
+                break;
+            case "C":
+                $('#JournalText').val(templateC);
+                break;
+            case "Blank":
+                $('#JournalText').val('');
+                break;
+        }
+    });
+});
---
+++ MyJournal/wwwroot/js/templates/templateA.js
@@ -0,0 +1,14 @@
+﻿let templateA = 
+`What did I achieve today ?
+
+What lessons did I learn today ?
+
+What am I thankful for right now ?
+
+How am I feeling right now ?
+
+What did I read today ?
+
+What are three amazing things that happened today ?
+
+How could I have made today better ?`
