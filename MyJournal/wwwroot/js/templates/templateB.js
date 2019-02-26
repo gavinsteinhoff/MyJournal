74cb990045227fb14e1f0ab24c8c2d2d8913b832
Gavin Steinhoff <gavinsteinhoff@gmail.com>
2019-02-26 14:02:45 -0600

    edit

--- MyJournal/wwwroot/js/templates/templateB.js
+++ MyJournal/wwwroot/js/templates/templateB.js
@@ -3,21 +3,8 @@
         text:
              `I am grateful for ...
 
-1. My positive and peaceful attitude
-
-2. Having the oppertunity to attend college
-
-3. Peace in our world
-
-            What will I do to make today great?
-
-1. Win the Mega Millions ;-)
-
-2. Going somewhere new
-
-3. Start learning a new language 
-
-4. Spend the evening with my family`,
+What will I do to make today great?
+`,
         name: 'template B'
     }
 )
